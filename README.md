761f78002b4ad047f27f070ad099e9213a68cb85
Alain Brenzikofer <alain@integritee.network>
2026-02-01 12:31:57 +0100

    phase 1 and 2 working

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 
 
+litescan mongodb query:
+`{method: "remark", "args.remark": { $regex: /^CoReVo:/ }}`
 
 
+Add or update metadata for different chains
+```
+cargo install subxt-cli
 subxt metadata  --url wss://polkadot-asset-hub-rpc.polkadot.io:443 > polkadot_asset_hub_metadata.scale
 subxt metadata  --url wss://sys.ibp.network/asset-hub-kusama:443 > kusama_asset_hub_metadata.scale
 subxt metadata  --url wss://sys.ibp.network/asset-hub-paseo:443 > paseo_asset_hub_metadata.scale
 subxt metadata  --url wss://collectives-paseo.rpc.amforc.com:443 > paseo_collectives_metadata.scale
+```
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use std::str::FromStr;
 use subxt::{
-    utils::{AccountId32, MultiAddress},
     OnlineClient, PolkadotConfig,
+    utils::{AccountId32, MultiAddress},
 };
-use subxt_signer::sr25519::dev::{self};
-use futures::StreamExt;
 use subxt_signer::{ExposeSecret, SecretUri};
 use subxt_signer::sr25519::Keypair;
 use rand::{random};
 use crypto_box::{PublicKey as BoxPublicKey, SecretKey as BoxSecretKey, ChaChaBox, aead::{Aead, AeadCore, OsRng}};
-use x25519_dalek::{StaticSecret, PublicKey as X25519PublicKey};
+use x25519_dalek::{StaticSecret, PublicKey as X25519PublicKey, SharedSecret};
 use sha2::{Sha512, Digest};
+use codec::{Decode, Encode, Output, Input};
+use futures::future::join_all;
+use futures::{stream, StreamExt};
 
 #[subxt::subxt(runtime_metadata_path = "paseo_people_metadata.scale")]
 pub mod assethub {}
@@ -19,11 +20,83 @@ pub mod assethub {}
 // but PolkadotConfig is a little more correct, having the right `Address` type.
 type AssetHubConfig = PolkadotConfig;
 
-pub struct VotingAccount {
+/// we prefix our remarks with a unique byte sequence to identify them easily.
+/// Remember that the version will be the prefix of the prefix in the final remark.
+/// For V1 the remark will start with: `0x00<PREFIX>`
+const COREVO_REMARK_PREFIX: [u8; 3] = hex_literal::hex!("c07030");
+const CONTEXT: &str = "corevo_test_voting";
+
+struct VotingAccount {
     pub sr25519_keypair: Keypair,
     pub x25519_public: X25519PublicKey,
     pub x25519_secret: StaticSecret,
 }
+
+// ensure backwards compatibility if we can migrate our message formats in the future
+#[derive(Encode, Decode, Debug, PartialEq, Eq, Clone)]
+enum CorevoRemark {
+    V1(CorevoRemarkV1)
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct CorevoRemarkV1 {
+    pub context: Vec<u8>,
+    pub msg: CorevoMessage
+}
+
+/// for easy filtering, we prefix the encoded remark
+impl Encode for CorevoRemarkV1 {
+    fn encode_to<T: Output + ?Sized>(&self, dest: &mut T) {
+        dest.write(&COREVO_REMARK_PREFIX);
+        self.context.encode_to(dest);
+        self.msg.encode_to(dest);
+    }
+
+    fn size_hint(&self) -> usize {
+        COREVO_REMARK_PREFIX.len() + self.context.size_hint() + self.msg.size_hint()
+    }
+}
+
+impl Decode for CorevoRemarkV1 {
+    fn decode<I: Input>(input: &mut I) -> Result<Self, codec::Error> {
+        let mut prefix = [0u8; 3];
+        input.read(&mut prefix)?;
+        if prefix != COREVO_REMARK_PREFIX {
+            return Err("invalid Corevo remark prefix".into());
+        }
+        let context = Vec::<u8>::decode(input)?;
+        let msg = CorevoMessage::decode(input)?;
+        Ok(CorevoRemarkV1 { context, msg })
+    }
+}
+
+#[derive(Encode, Decode, Debug, PartialEq, Eq, Clone)]
+enum CorevoMessage {
+    /// tell the world your X25519 pubkey so anyone can send you encrypted messages
+    AnnounceOwnPubKey([u8; 32]),
+    /// Invite a voter to participate and sher a common salt for the group
+    InviteVoter(AccountId32, [u8; 32]),
+    /// Commit your salted vote hash and persist the [`CorevoSaltedVote`], encrypted to yourself
+    Commit([u8; 32], Vec<u8>),
+    /// Reveal your salted vote to those who know the common salt
+    Reveal(CorevoSaltedVote),
+}
+
+#[derive(Encode, Decode, Debug, PartialEq, Eq, Clone)]
+struct CorevoSaltedVote {
+    pub vote: String,
+    pub salt: [u8; 32]
+}
+
+#[derive(Encode, Decode, Debug, PartialEq, Eq, Clone)]
+enum CorevoVote {
+    Aye,
+    Nay,
+    Abstain,
+    Other(String)
+}
+
+
 
 #[tokio::main]
 pub async fn main() {
@@ -41,13 +114,7 @@ async fn run() -> Result<(), Box<dyn std::error::Error>> {
         derive_account(&api, "//KvPoPperA"),
         derive_account(&api, "//KvPoPperB")
     )?;
-    let common_salt = random::<[u8; 32]>();
-    println!("common salt: {:?}", common_salt);
-    let ciphertext = encrypt_for_recipient(&proposer.x25519_secret, &voter_b.x25519_public, &common_salt).unwrap();
-    println!("Encrypted message from proposer to voter B: {:?}", ciphertext);
-    let plaintext_at_voter_b = decrypt_from_sender(&voter_b.x25519_secret, &proposer.x25519_public, ciphertext.as_slice()).unwrap();
-    println!("Decrypted message at voter B: {:?}", plaintext_at_voter_b);
-
+    let everybody = vec![&proposer, &voter_b];
 
     let api_for_blocks = api.clone();
     let listener_handle = tokio::spawn(async move {
@@ -56,6 +123,33 @@ async fn run() -> Result<(), Box<dyn std::error::Error>> {
         }
     });
     println!("Listening to System.Remark Extrinsics in new finalized blocks...");
+
+    println!("*********** SETUP PHASE **************" );
+    // Every voter publishes their X25519 public key on-chain using System.Remark
+    let _ = join_all(everybody.iter().map(|signer|
+        send_remark(&api, &signer.sr25519_keypair,
+                    CorevoRemark::V1(CorevoRemarkV1 {
+                        context: CONTEXT.as_bytes().to_vec(),
+                        msg: CorevoMessage::AnnounceOwnPubKey(signer.x25519_public.to_bytes())
+                    })))).await;
+    println!("*********** INVITE PHASE **************" );
+    let common_salt = random::<[u8; 32]>();
+    println!("common salt: {}", hex::encode(common_salt.encode()));
+    let ciphertext = encrypt_for_recipient(&proposer.x25519_secret, &voter_b.x25519_public, &common_salt).unwrap();
+    println!("    verify: Encrypted message from proposer to voter B: 0x{}", hex::encode(ciphertext.encode()));
+    let plaintext_at_voter_b = decrypt_from_sender(&voter_b.x25519_secret, &proposer.x25519_public, ciphertext.as_slice()).unwrap();
+    println!("    verify: Decrypted message at voter B: 0x{}", hex::encode(plaintext_at_voter_b.encode()));
+
+    // send encrypted common salt to everybody else (and self, for persistence). Send sequentially to avoid nonce race.
+    for account in everybody {
+        send_remark(&api, &proposer.sr25519_keypair, CorevoRemark::V1(CorevoRemarkV1 {
+            context: CONTEXT.as_bytes().to_vec(),
+            msg: CorevoMessage::InviteVoter(
+                account.sr25519_keypair.public_key().to_account_id(),
+                common_salt
+            )
+        })).await?
+    }
 
     listener_handle.await?;
     Ok(())
@@ -100,21 +194,34 @@ async fn listen_to_blocks(api: OnlineClient<AssetHubConfig>) -> Result<(), Box<d
         let block_number = block.number();
         let block_hash = block.hash();
 
-        println!("Block #{block_number}:");
-        println!("  Hash: {block_hash}");
-        println!("  Extrinsics:");
-
         let extrinsics = block.extrinsics().await?;
 
         for ext in extrinsics.iter() {
             if let Some(remark) = ext.as_extrinsic::<assethub::system::calls::types::Remark>()? {
-                println!("Remark: {:?}", remark);
+                println!(">>> Remark in block {}: 0x{}", block.number(), hex::encode(remark.remark));
             }
-            if let Some(transfer) = ext.as_extrinsic::<assethub::balances::calls::types::TransferKeepAlive>()? {
-                println!("Transfer: {:?}", transfer);
-            }
+            //if let Some(transfer) = ext.as_extrinsic::<assethub::balances::calls::types::TransferKeepAlive>()? {
+            //    println!("Transfer: {:?}", transfer);
+            //}
         }
     }
+    Ok(())
+}
+
+async fn send_remark(api: &OnlineClient<AssetHubConfig>, signer: &Keypair, remark: CorevoRemark) -> Result<(), Box<dyn std::error::Error>> {
+    let remark_bytes = remark.encode();
+
+    let remark_tx = assethub::tx()
+        .system()
+        .remark(remark_bytes);
+    let _events = api
+        .tx()
+        .sign_and_submit_then_watch_default(&remark_tx, signer)
+        .await?
+        .wait_for_finalized_success()
+        .await?;
+
+    println!("Remark sent by {}: {:?}", signer.public_key().to_account_id(), remark);
     Ok(())
 }
 
